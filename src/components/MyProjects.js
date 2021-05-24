077392daa4b598f5d7508f735f1b9e7b96be971a
Eric <22939634+ericDevSantana@users.noreply.github.com>
2021-05-23 17:34:12 -0700

    my projects filter

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -6,11 +6,32 @@ const MyProjects = (props) => {
     <div className={styles.myProjects}>
       <h1>my projects</h1>
       <div className={styles["myProjects-list"]}>
+        <div className={styles["myProjects-filter"]}>
+          <a href="/">all</a>
+          <a href="/">react.js</a>
+          <a href="/">javascript</a>
+          <a href="/">html</a>
+          <a href="/">css</a>
+        </div>
+      </div>
+      <div className={styles["myProjects-list"]}>
         <div className={styles["myProjects-item"]}>
           <h3>Project 1</h3>
         </div>
         <div className={styles["myProjects-item"]}>
-          <h3>Project 1</h3>
+          <h3>Project 2</h3>
+        </div>
+        <div className={styles["myProjects-item"]}>
+          <h3>Project 3</h3>
+        </div>
+        <div className={styles["myProjects-item"]}>
+          <h3>Project 4</h3>
+        </div>
+        <div className={styles["myProjects-item"]}>
+          <h3>Project 5</h3>
+        </div>
+        <div className={styles["myProjects-item"]}>
+          <h3>Project 6</h3>
         </div>
       </div>
     </div>
