a0a6aa146e273b73c08e7b86cb18f83afaac9c51
Eric <22939634+ericDevSantana@users.noreply.github.com>
2021-05-26 00:58:41 -0700

    projectCard component

--- src/Layout/ProjectCard.js
+++ src/Layout/ProjectCard.js
@@ -4,16 +4,20 @@ import styles from "./ProjectCard.module.css";
 
 const ProjectCard = (props) => {
   return (
-    <a href="/" key={props.data.id} className={styles["projectCard-item"]}>
+    <a
+      href={props.data.href}
+      key={props.data.id}
+      className={styles["projectCard-item"]}
+    >
       <img
         className={styles["projectCard-item-img"]}
-        src="project-bg.jpeg"
-        alt="landing page"
+        src={props.data.url}
+        alt={props.data.alt}
       />
       <div className={styles["projectCard-item-description"]}>
         <h2>{props.data.name}</h2>
         <p>tag: {props.data.tag}</p>
-        <p>this is a little description about this project. </p>
+        <p>{props.data.description}</p>
       </div>
     </a>
   );
